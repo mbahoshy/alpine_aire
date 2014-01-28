d1f11cb258301c61de6f7dc6694ff4866393040c
Matt <mbahoshy@gmail.com>
2014-01-27 20:27:51 -0800

    index page styling. added new text to index page

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<title>Alpine Aire Heating and Cooling | Wenatchee Furnace and AC Repair</title>
 		<!-- metatags -->
 			<meta name='description' content='Welcome to Alpine Aire Heating and Cooling! We provide residential and commerical HVAC repair and replacement to the greater Wenatchee area.'>
-			<meta name='keywords' content='Alpine Aire, Wenatchee HVAC Services, Alpine air, air conditioning repair, ac repair, ductless air conditioning'>
+			<meta name='keywords' content='Alpine Aire, Wenatchee HVAC Services, Wenatchee heating company, Alpine air, air conditioning repair, ac repair, ductless air conditioning'>
 			<meta http-equiv=”Content-Type” content=”text/html; charset=ISO-8859-1?>
 			<meta name='viewport' content='width=device-width, initial-scale=.5, maximum-scale=1'>
 			
@@ -133,60 +133,57 @@
 				<li><a class='navun' id='contact' href='pages/contact-alpine-aire.php'>Contact</a></li>
 			</ul>
 		</div>
-		<div id='main' class='white'>
 			<div class='title_bar'>
 				<h1><span class='b_name'>Alpine Aire Heating and Cooling Inc.</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#124;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class='tagline'><i>Breathe Naturally</i></span></h1>
 			</div>
 			<div class='content_box'>
 				<div id="slideshow">
-					<img src="images/alpine-slideshow-1.jpg" alt="Heat Pump Installation" class="active" />
-					<img src="images/alpine-slideshow-2.jpg" alt="Air Quality Comfort" />
-					<img src="images/alpine-slideshow-3.jpg" alt="HVAC New Construction Job" />
+					<img src="images/alpine-slideshow-1.jpg" alt="Heat Pump Installation" class="active round" />
+					<img src="images/alpine-slideshow-2.jpg" alt="Air Quality Comfort" class='round'/>
+					<img src="images/alpine-slideshow-3.jpg" alt="HVAC New Construction Job" class='round'/>
 				</div>
-				<div class='top_text'>
-					<p class='center'><i>Proudly serving our family and friends in North Central Washington since 1986.</i></p>
-					<!-- <p class='center'>Alpine Aire performs HVAC service in the following areas:</p> -->
-						<ul class='fleft'>
-							<li>Wenatchee Valley</li>
-							<li>Cashmere</li>
-							<li>Leavenworth</li>
-							<li>Entiat</li>
-							<li>Quincy</li>
-						</ul>
-						<ul class='fright'>
-							<li>Rock Island</li>
-							<li>Waterville</li>
-							<li>Cle Elum</li>
-							<li>Plain</li>
-							<li>Lake Wenatchee</li>
-						</ul>
+				<div class='absolute'>
+					<div class='top_text'>
+						<p class='center'><i>Proudly serving our family and friends in North Central Washington since 1986.</i></p>
+						<!-- <p class='center'>Alpine Aire performs HVAC service in the following areas:</p> -->
+							<ul class='fleft'>
+								<li>Wenatchee Valley</li>
+								<li>Cashmere</li>
+								<li>Leavenworth</li>
+								<li>Entiat</li>
+								<li>Quincy</li>
+							</ul>
+							<ul class='fright'>
+								<li>Rock Island</li>
+								<li>Waterville</li>
+								<li>Cle Elum</li>
+								<li>Plain</li>
+								<li>Lake Wenatchee</li>
+							</ul>
+					</div>
 				</div>
 			</div>
+		<div id='main' class='white'>
+
 
 			<!-- Schedule online -->
 			<div class='servicebox fleft'>
 				<form name='contact_form' class='fleft' action='pages/contact_action.php' onsubmit="return validateForm();" method="post">
-					<h2>Need HVAC Service Now? -- Schedule Fast and Easy Online!</h2>
-					<p><i>Name:</i><br	/><input class='i_length' type="text" name="cf_name"></p>
-					<p><i>Email Address:</i><br	/><input class='i_length' type="text" name="cf_email"></p>
-					<p><i>Phone Number:</i>&nbsp;&nbsp;&nbsp;<input type='text' maxlength='10' name='cf_phone'></p>
-					<p><i>Please briefly describe your problem:</i></p>
+					<h2>Schedule Fast and Easy Online!</h2>
+					<p><label><i>Name:</i></label><br	/><input class='i_length' type="text" name="cf_name"></p>
+					<p><label><i>Email Address:</i></label><br	/><input class='i_length' type="text" name="cf_email"></p>
+					<p><label><i>Phone Number:</i></label>&nbsp;&nbsp;&nbsp;<input type='text' maxlength='10' name='cf_phone'></p>
+					<p><label><i>Please briefly describe your problem:</i></label></p>
 					<p><textarea maxlength='500' name="cf_message"></textarea></p>
 						<input type="submit" value="Send">
 						<input type="reset" value="Clear">
 				</form>
-				<div class='sbox_text fleft'>
-					<h2>Don't take our word... <span class='underline'>Alpine Aire service speaks for itself !</span></h2>
-					<div class='merits'>
-						<p><span class='merit_h'>"I was so very impressed </span>with the <b>promptness and quality</b> of service I received. <!--We've always been very pleased with the work we've had done from Alpine Aire, but yesterday's service experience was phenomenal.--> We will gladly recommend your company to anyone we can. Thank you for having such a wonderful crew working with you!<span class='merit_name'>-Sheli from Wenatchee<span class='merit_h'>"</span></span></p>
-						<!-- <p><span class='merit_h'>Quality - </span>The foremost goal of every HVAC service call, air conditioning repair, or replacement installation is <i>quality</i>. Located in the the heart of Wenatchee, we know the temperature extremes that our customers (and their HVAC systems) face on a daily basis. That is why we are committed to solving furnace repair and AC repair calls promptly and effectively. <b>We will repair your HVAC system right the first time</b>.</p>-->
-					</div>
-					<div class='merits'>
-						<p><span class='merit_h'>"Miguel went the extra mile... </span>He explained everything clearly, and made excellent recommendations for maintaining the health of the system. His communication style was very professional and customer focused. He demonstrated a high level of HVAC system technical expertise. On a grading scale of A through F, <b>I would give him an A+.</b> <span class='merit_name'>-Chris from Wenatchee<span class='merit_h'>"</span></span></p>
-					</div>
-					<div class='merits'>
-						<p><span class='merit_h'>"The new infinity system </span>is much much quieter than the old system. The first morning it was installed, I actually walked outside to see if the heat pump was working; <b>it was that quiet.</b> I want to thank you and your team for their professionalism and exprtise in installing the system.<span class='merit_name'>-Henry from East Wenatchee<span class='merit_h'>"</span></span></p>
-					</div>
+				<div class='merits'>
+<!-- 						<p><span class='merit_h'>"I was so very impressed </span>with the <b>promptness and quality</b> of service I received. We've always been very pleased with the work we've had done from Alpine Aire, but yesterday's service experience was phenomenal. We will gladly recommend your company to anyone we can. Thank you for having such a wonderful crew working with you!<span class='merit_name'>-Sheli from Wenatchee<span class='merit_h'>"</span></span></p> -->
+
+						<p>Alpine Aire offers the best customer experience possible for HVAC service, maintenance and repairs. To us, the customer experience means a few things.</p>
+						<p>Most importantly, we want the customer to feel comfortable. From the air coming out of your vents, to a friendly smile at the door. Alpine Aire staff are trained to put the customer first.</p>
+						<p>When a furnace or air conditioner breaks dows in North Central Washington, it needs to get fixed right the first time. Our knowledgeable HVAC service technicians can diagnose any make or model of heat pump, furnace, or air conditioner that runs your home.</p>
 				</div>
 			</div>
 			
@@ -196,18 +193,22 @@
 					<ul>
 						<a href='pages/residential-repair-or-replace.php' ><img class='fleft' src='images/new_system.png'	/><li>Repair or Replace?</li></a>
 						<a href='pages/residential-filter-maintenance.php' ><img class='fleft' src='images/filter_cabinet.png'	/><li>How often should I change my filter?</li></a>
-						<a href='pages/residential-air-quality.php' ><img class='fleft' src='images/infinity_air_purifier.png'	/><li>How can I remove smoke and other odor causing particles?</li></a>
+						<a href='pages/residential-air-quality.php' ><img class='fleft' src='images/infinity_air_purifier.png'	/><li>How can I remove odor causing particles?</li></a>
 						<a href='pages/residential-energy-efficient.php' ><img class='fleft' src='images/energystar.png'	/><li>What are my Energy Efficient options?</li><a>
 						<a href='pages/residential-financing.php' ><img class='fleft' src='images/financing.png'	/><li>Do you have financing?</li></a>
 					</ul>
 			</div>
 			<div class='lrg_text fright green border_bottom'>
-				
-				<img class='fleft padright' src='images/infinity_20.png'	/>
-				<h3>the most <i>efficient</i> ducted, air source dual fuel system available. <a target="_blank" href='http://www.carrier.com/homecomfort/en/us/innovation/#1'>Learn more</a></h3>
-				<img class='fright padright' src='images/greenspeed.png'	/>
+				<div class='sbox_text fleft'>
+					<h2>Don't take our word... <span class='underline'>Alpine Aire service speaks for itself !</span></h2>
+					<img src='images/alpine-aire-hvac-service-techs.jpg'	/>
+
+
+				</div>				
+
 			</div>
-			<div class='lrg_text fright'>
+			<div class='clear'></div>
+			<div class='full-text fright'>
 				<h2><i>Our HVAC service department specializes in:</i></h2>
 					<div class='btest'>
 					<ul class='fleft'>
