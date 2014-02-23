6838bb8ade13f4fda89c009fc98b5eb44629c1f0
Matt <mbahoshy@gmail.com>
2014-02-23 09:54:21 -0800

    modified index.html

--- index.php
+++ index.php
@@ -235,7 +235,7 @@
 		<!-- footer -->
 		<div itemscope itemtype="http://www.schema.org/LocalBusiness">
 			<h5><span itemprop="name">Alpine Aire Heating and Cooling Inc.</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<span itemprop="telephone">509.662.6846</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<span itemprop="address">1306 Walnut St. Wenatchee, WA</span></h5>
-			<h5><a href='../mobile.php'>Mobile Site</a>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a href='../pages/contact-alpine-aire.php'>Contact</a>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a href='../pages/alpine-aire-hvac-jobs.php'>Jobs</a>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_blank" href='http://www.facebook.com/pages/Alpine-Aire-Heating-Cooling/565406576819846'>Like us on Facebook!</a></h5>
+			<h5><a href='../mobile.php'>Mobile Site</a>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a href='../pages/contact-alpine-aire.php'>Contact</a>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a href='pages/alpine-aire-hvac-jobs.php'>Jobs</a>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_blank" href='http://www.facebook.com/pages/Alpine-Aire-Heating-Cooling/565406576819846'>Like us on Facebook!</a></h5>
 			
 		</div>
 	
