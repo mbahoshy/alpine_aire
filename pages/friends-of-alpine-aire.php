df1f87d1b74a359a0db0b0accc3b11aa8f33564b
Matt <mbahoshy@gmail.com>
2014-02-23 11:02:26 -0800

    styled friends of alpine links

--- pages/friends-of-alpine-aire.php
+++ pages/friends-of-alpine-aire.php
@@ -30,13 +30,35 @@
 			</div>
 
 			<div class='subnavtext'>
-				<img  class='team-pic' src='../images/alpine-aire-hvac-service-techs.jpg'	/>
-				
-				<p>Since 1986 Alpine Aire has been serving residents of the greater Wenatchee Valley. Over the years, our reputation has helped us become a trusted provider for homeowners in Wenatchee, WA.</p>
-				<p>Wenatchee customers rely on Alpine Aire to provide quality heating and cooling repairs no matter what the season. It is our priority to keep our friends and family safe and comfortable through the winter and summer months.</p>
-				<p>We offer all kinds of services to our Wenatchee customers, <i>emergency hvac repair</i>, <i>preventative maintenance</i>, and <i>new system installation</i>.</p>
-				<p><b>Need an HVAC contractor?</b> Alpine Aire installs custom HVAC systems in Wenatchee year around. Forced air, AC, heat pumps, radiant in-floor, gas piping, heat recovery ventilators, chillers, boilers... we handle it all!</p>
-				
+				<p>Alpine Aire supports the local community!</p>
+				<ul class='alpine-friends'>
+					<li><a href='http://www.ahcconsulting.net' target="_blank" >After Hourse Computer Consulting</a><p>Computers, Servers</p><span>509-630-6312</span></li>
+					<li><a href='http://www.aqcpaint.com' target="_blank" >American Quality Coatings</a><p>Painting, Staining</p><span>509-663-1300</span></li>
+					<li><a href='http://www.columbiahc.net' target="_blank" >Columbia Home Care</a><p>Eldery Care</p><span>509-470-8554</span></li>
+					<li><a href=''>Columbia River Interiors</a><p>Carpet, Laminate, Tile</p><span>509-665-9944</span></li>
+					<li><a href='http://www.countryfinancial.com' target="_blank" >County Financial</a><p>Insurance</p><span>509-663-3800</span></li>
+					<li><a href=''>Fuze Salon</a><p>Salon</p><span>509-888-2242</span></li>
+					<li><a href='http://www.saltmancpa.com' target="_blank" >Saltman CPA</a><p>CPA</p><span>509-667-1754</span></li>
+					<li><a href='http://www.jpportraitstudio.com' target="_blank" >JP Portrait Studio</a><p>Photographers</p><span>509-884-2911</span></li>
+					<li><a href='http://www.tracidry.com' target="_blank" >Mortgage Advisory Group</a><p>Mortgage</p><span>509-470-7800</span></li>
+					<li><a href=''>PANE-Less Glass Cleaning</a><p>Window Cleaning</p><span>509-860-6720</span></li>
+					<li><a href='http://www.estwenatcheedentist.com' target="_blank" >Riverview Family Dental</a><p>Dental</p><span>509-884-7137</span></li>
+					<li><a href='http://www.cascadeautocenter.com' target="_blank" >Cascade Auto Center</a><p>Auto Sales</p><span>509-663-5699</span></li>
+					<li><a href='http://www.mycmsite.com/dbenbow' target="_blank" >Creative Memories</a><p>Photo Solutions</p><span>509-662-6752</span></li>
+					<li><a href=''>Steve Sells Your Stuff</a><p>Seller</p><span>509-881-8897</span></li>
+					<li><a href='http://www.warminteriors.com' target="_blank" >Warm Elements</a><p>Design &amp; Color Consultation</p><span>509-548-5965</span></li>
+					<li><a href='http://www.leavenworthelectricandexcavation.com' target="_blank" >Leavenworth Electric</a><p>Electric &amp; Excavation</p><span>509-433-1800</span></li>
+					<li><a href='http://www.faithstepscleaning.com' target="_blank" >Faithsteps Housecleaning</a><p>Cleaner</p><span>509-884-7614</span></li>
+					<li><a href='http://www.edwardjones.com' target="_blank" >Edward Jones</a><p>Financial Advisor</p><span>509-886-1106</span></li>
+					<li><a href='http://www.crawfordcarpetcare.com' target="_blank" >Crawford Carpet Care</a><p>Carpet Cleaning</p><span>509-662-3526</span></li>
+					<li><a href='http://www.homesinwenatchee.com' target="_blank" >Coldwell Banker</a><p>Realtor</p><span>509-630-0235</span></li>
+					<li><a href='http://www.bearsigns.net' target="_blank" >Bear Signs &amp; Graphics</a><p>Signs &amp; Graphics</p><span>509-264-1349</span></li>
+					<li><a href='http://www.wenatcheechiropractor.com' target="_blank" >Banker Chiropratic Clinic</a><p>Chiropractic Clinic</p><span>509-630-5101</span></li>
+					<li><a href=''>Independent Health Agent</a><p>Health Insurance</p><span>509-670-2618</span></li>
+					<li><a href='http://www.measure-the-difference.com' target="_blank" >PHARMANEX</a><p>AGE LOC</p><span>509-663-7198</span></li>
+					<li><a href='http://www.digitalmedianw.com' target="_blank" >World Digital Media</a><p>Websites</p><span>509-661-5204</span></li>
+				</ul>
+
 			</div>
 		</div>
 		<?php include("../php_include/footer.php"); ?>
